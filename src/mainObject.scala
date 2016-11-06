813368d5c4b44481229a1deaf10a571fdddac0f5
leehwangchun <elluard@me.com>
2016-11-06 18:41:46 +0900

    #chage APIKey use

--- src/mainObject.scala
+++ src/mainObject.scala
@@ -11,6 +11,6 @@ object mainObject {
     val bank = new Bank()
 
     //Insert Key that you can get https://account.arena.net/applications
-    bank.Request("8A4C1D8D-6188-FB46-B0F3-5492CC0841AE3717C229-C7C3-4BFD-9D44-8B4656FF70E1")
+    bank.Request()
   }
 }
