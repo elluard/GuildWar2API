bd8cf4f849da2c390b5fa2eba583a0a753b24016
leehwangchun <elluard@me.com>
2016-11-06 18:41:03 +0900

    #chage APIKey use

--- src/mainObject.scala
+++ src/mainObject.scala
@@ -8,9 +8,9 @@ object mainObject {
   def main(args : Array[String]): Unit = {
     val accountObj = new Account()
     val characterObj = new Character()
+    val bank = new Bank()
 
     //Insert Key that you can get https://account.arena.net/applications
-    accountObj.Request("")
-    characterObj.Request("")
+    bank.Request("8A4C1D8D-6188-FB46-B0F3-5492CC0841AE3717C229-C7C3-4BFD-9D44-8B4656FF70E1")
   }
 }
